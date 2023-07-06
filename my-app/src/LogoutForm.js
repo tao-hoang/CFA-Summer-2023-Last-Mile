2f5038415646f309df71267395e279a6042afdbf
Tao Hoang <taohoang01@gmail.com>
2023-07-06 14:46:36 -0700

    added logout form

--- my-app/src/LogoutForm.js
+++ my-app/src/LogoutForm.js
@@ -6,5 +6,15 @@ const LogoutForm = () => {
     localStorage.removeItem('token');
 
     // Redirect to the landing page or any other desired location
-    // You can use React Router for
+    // You can use React Router for navigation
+    // history.push('/');
+  };
 
+  return (
+    <div>
+      <h1>Logout</h1>
+      <p>Click the button below to logout:</p>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
