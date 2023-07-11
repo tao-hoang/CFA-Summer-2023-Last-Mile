af657c565dbae8b24556eff6f1bcfe59b87c170e
Tao Hoang <taohoang01@gmail.com>
2023-07-11 14:03:33 -0700

    added search bar

--- my-app/src/LandingPage.js
+++ my-app/src/LandingPage.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LandingPage = () => {
-return (
-<div>
-<h1>Welcome to Our App!</h1>
-  <p>We made this app to help people learn skills and find jobs in places that they love!</p>
-<h2>Features:</h2>
+  const [searchQuery, setSearchQuery] = useState('');
 
-<ul>
-    <li>Search for jobs based on location, skills, and preferences</li>
-    <li>Explore a wide range of industries and career paths</li>
-    <li>Access comprehensive resources and learning materials</li>
-    <li>Connect with industry professionals and mentors</li>
-    <li>Get personalized job recommendations</li>
-    <li>Track your learning progress and achievements</li>
-</ul>
-  <h2>How It Works:</h2>
-    <ol>
-      <li>Create an account to get started</li>
-      <li>Set your preferences and indicate your desired skills and location</li>
-      <li>Explore the available job listings and learning resources</li>
-      <li>Apply for jobs that match your interests and qualifications</li>
-      <li>Connect with other users and industry professionals to expand your network</li>
-      <li>Track your job applications and interview progress</li>
-      <li>Continuously learn and enhance your skills to improve your career prospects</li>
-    </ol>
-      <h2>Join Our Community Today!</h2>
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // Handle search query submission
+    // You can perform any search-related logic here
+    console.log('Search submitted:', searchQuery);
+  };
+
+  return (
+    <div>
+      <h1>Welcome to Our App!</h1>
+      <p>We made this app to help people learn skills and find jobs in places that they love!</p>
+      <h2>Features:</h2>
+      {/* Existing features list */}
+      <ul>
+        <li>Search for jobs based on location, skills, and preferences</li>
+        <li>Explore a wide range of industries and career paths</li>
+        <li>Access comprehensive resources and learning materials</li>
+        <li>Connect with industry professionals and mentors</li>
+        <li>Get personalized job recommendations</li>
+        <li>Track your learning progress and achievements</li>
+      </ul>
+      {/* Search bar */}
+      <form onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          placeholder="Search jobs and courses..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      
+      {/* How It Works section */}
+      <ol>
+        <li>Create an account to get started</li>
+        <li>Set your preferences and indicate your desired skills and location</li>
+        <li>Explore the available job listings and learning resources</li>
+        <li>Apply for jobs that match your interests and qualifications</li>
+        <li>Connect with other users and industry professionals to expand your network</li>
+        <li>Track your job applications and interview progress</li>
+        <li>Continuously learn and enhance your skills to improve your career prospects</li>
+      </ol>
+      {/* Join Our Community section */}
       <p>Sign up now to start your journey towards a fulfilling and successful career!</p>
     </div>
   );
