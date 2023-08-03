4684d25a9e70ea5a8b61908e76180d3f44769d8a
Tao Hoang <taohoang01@gmail.com>
2023-08-03 14:34:03 -0700

    imported material ui and added navbar

--- my-app/src/components/Homepage.js
+++ my-app/src/components/Homepage.js
@@ -56,6 +56,8 @@ const cardsData2 = [
   { id: 3, title: 'Card 3', description: 'This is the description for Card 3' },
 ];
 
+
+
 const HomePage = () => {
   const [activeCardIndex1, setActiveCardIndex1] = useState(0);
   const [activeCardIndex2, setActiveCardIndex2] = useState(0);
@@ -81,47 +83,81 @@ const HomePage = () => {
   };
 
   return (
-    <div className="homepage">
-      <h1>Welcome to connectIT</h1>
-      <div className="card-section">
-        <h2>Section 1</h2>
-        <div className="card-container">
-          {cardsData1.map((card, index) => (
-            <div
-              className={`card ${index === activeCardIndex1 ? 'active' : ''}`}
-              key={card.id}
-            >
-              <h3>{card.title}</h3>
-              <p>{card.description}</p>
-              <div className="arrows">
-                <span onClick={() => handleArrowClick('left', 1)}>&#x2190;</span>
-                <span onClick={() => handleArrowClick('right', 1)}>&#x2192;</span>
-              </div>
+    <div className='landingBodyContainer'>
+      <LandingNav showLinks="true" />
+      <ThemeProvider theme={theme}>
+        <div className='landingBody'>
+          <div id="heroImage">
+            <div className="welcomeText">
+              <h1 className="welcomeHeader" style={{ marginBottom: 20 }}>Work to build yourself up.</h1>
+              <p className="welcomeMessage" style={{ marginBottom: 50, fontSize: 19, fontFamily: "lora" }}>This is where you can find available jobs and recommended courses to learn new skills and build your portfolio.</p>
+              <p className='searchPrompt'>What kind of work are you looking for?</p>
+              <Paper onSubmit={handleSearchSubmit}
+                component="form"
+                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 375, opacity: 0.5, borderRadius: 2 }}
+              >
+                <IconButton sx={{ p: '10px' }} aria-label="menu">
+                  <MenuIcon />
+                </IconButton>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  inputProps={{ 'aria-label': 'search' }}
+                  type="text"
+                  placeholder="Search jobs and courses..."
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                />
+                <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                  <SearchIcon />
+                </IconButton>
+              </Paper>
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
 
-      <div className="card-section">
-        <h2>Section 2</h2>
-        <div className="card-container">
-          {cardsData2.map((card, index) => (
-            <div
-              className={`card ${index === activeCardIndex2 ? 'active' : ''}`}
-              key={card.id}
-            >
-              <h3>{card.title}</h3>
-              <p>{card.description}</p>
-              <div className="arrows">
-                <span onClick={() => handleArrowClick('left', 2)}>&#x2190;</span>
-                <span onClick={() => handleArrowClick('right', 2)}>&#x2192;</span>
+          <div className="homepage">
+            <h1>Welcome to connectIT</h1>
+            <div className="card-section">
+              <h2>Section 1</h2>
+              <div className="card-container">
+                {cardsData1.map((card, index) => (
+                  <div
+                    className={`card ${index === activeCardIndex1 ? 'active' : ''}`}
+                    key={card.id}
+                  >
+                    <h3>{card.title}</h3>
+                    <p>{card.description}</p>
+                    <div className="arrows">
+                      <span onClick={() => handleArrowClick('left', 1)}>&#x2190;</span>
+                      <span onClick={() => handleArrowClick('right', 1)}>&#x2192;</span>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          ))}
+
+            <div className="card-section">
+              <h2>Section 2</h2>
+              <div className="card-container">
+                {cardsData2.map((card, index) => (
+                  <div
+                    className={`card ${index === activeCardIndex2 ? 'active' : ''}`}
+                    key={card.id}
+                  >
+                    <h3>{card.title}</h3>
+                    <p>{card.description}</p>
+                    <div className="arrows">
+                      <span onClick={() => handleArrowClick('left', 2)}>&#x2190;</span>
+                      <span onClick={() => handleArrowClick('right', 2)}>&#x2192;</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     </div>
   );
 };
 
-export default HomePage;
+export default Homepage;
